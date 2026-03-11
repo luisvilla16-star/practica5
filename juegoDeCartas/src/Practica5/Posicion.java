049d6caa8ccf246e97d27ec39355a17a2cd11cd4
luis gabriel villa coronilla <luis.villa16@uabc.edu.mx>
2026-03-11 10:43:10 -0700

    brach nuevo

---
+++ juegoDeCartas/src/Practica5/Posicion.java
@@ -0,0 +1,23 @@
+package Practica5;
+
+public class Posicion {
+    int x;
+    int y;
+
+    public Posicion(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+    public int getX() {
+        return x;
+    }
+    public void setX(int x) {
+        this.x = x;
+    }
+    public int getY() {
+        return y;
+    }
+    public void setY(int y) {
+        this.y = y;
+    }
+}
---
+++ juegoDeCartas/src/Practica5/Visualizador.java
@@ -0,0 +1,9 @@
+package Practica5;
+
+public class Visualizador {
+    public static void carta(Carta c, Posicion p){
+
+    }
+
+
+}
